58f84bffd7b888eca17a6cb51675fb24faa596d7
Sebastian Venhuis <sebastian@venhuis.com>
2017-01-03 14:14:26 +0100

    Unity Project erstellt, Level Geladen, bewegung umgesetz

---
+++ ASCLabVisualizer/Assets/CommandExecutioner.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+struct Marker
+{
+    public int val;
+    public int dir;
+    public GameObject marker;
+    public bool stone;
+}
+
+public class CommandExecutioner : MonoBehaviour {
+    public GameObject markerPrefab;
+    public bool running = false;
+    public float timing = 1.0f;
+    public List<List<string>> commands;
+
+    List<Marker> markers;
+
+    public float timeSinceLastExecution = 0;
+
+	// Use this for initialization
+	void Start () {
+        markers = new List<Marker>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!running)//Do nothing if the component is turned of
+            return;
+        timeSinceLastExecution += Time.deltaTime;
+        if (timeSinceLastExecution < timing)//Return if we are still on cooldown
+            return;
+        timeSinceLastExecution = 0;
+        if(commands.Count == 0)
+        {
+            running=false;
+            return;
+        }
+        List<string> command = commands[0];
+        commands.RemoveAt(0);
+        int dir;
+        Vector3 mv;
+        Vector3 rot;
+        Marker marker;
+        switch(command[0])
+        {
+            case "STEP":
+                dir = int.Parse(command[1]);
+                mv = new Vector3();
+                if (dir == 0)
+                    mv.z += 1;
+                else if (dir == 1)
+                    mv.x += 1;
+                else if (dir == 2)
+                    mv.z -= 1;
+                else if (dir == 3)
+                    mv.x -= 1;
+                transform.Translate(mv, Space.World);
+                break;
+            case "MOVE":
+                dir = int.Parse(command[1]);
+                mv = new Vector3();
+                if (dir == 0)
+                    mv.z += 1;
+                else if (dir == 1)
+                    mv.x += 1;
+                else if (dir == 2)
+                    mv.z -= 1;
+                else if (dir == 3)
+                    mv.x -= 1;
+                transform.Translate(mv, Space.World);
+                if (dir == 0)
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                else if (dir == 1)
+                    transform.eulerAngles = new Vector3(0, 90, 0);
+                else if (dir == 2)
+                    transform.eulerAngles = new Vector3(0, 180, 0);
+                else if (dir == 3)
+                    transform.eulerAngles = new Vector3(0, 270, 0);
+
+                break;
+            case "LOOK":
+                dir = int.Parse(command[1]);
+                if (dir == 0)
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                else if (dir == 1)
+                    transform.eulerAngles = new Vector3(0, 90, 0);
+                else if (dir == 2)
+                    transform.eulerAngles = new Vector3(0, 180, 0);
+                else if (dir == 3)
+                    transform.eulerAngles = new Vector3(0, 270, 0);
+                break;
+            case "TELEPORT":
+                int telePosX = int.Parse(command[1]);
+                int telePosY = int.Parse(command[2]);
+                transform.position = new Vector3(telePosX, 0, -telePosY);
+                break;
+            case "SSTONE":
+                dir = int.Parse(command[1]);
+                mv = new Vector3();
+                if (dir == 0)
+                    mv.y -= 1;
+                else if (dir == 1)
+                    mv.x += 1;
+                else if (dir == 2)
+                    mv.y += 1;
+                else if (dir == 3)
+                    mv.x -= 1;
+                marker = new Marker();
+                marker.stone = true;
+                marker.marker = (GameObject)Instantiate(markerPrefab, mv, new Quaternion());
+                markers.Add(marker);
+                break;
+            case "SMARKER":
+                dir = int.Parse(command[1]);
+                mv = new Vector3();
+                if (dir == 0)
+                    mv.z += 1;
+                else if (dir == 1)
+                    mv.x += 1;
+                else if (dir == 2)
+                    mv.z -= 1;
+                else if (dir == 3)
+                    mv.x -= 1;
+                marker = new Marker();
+                marker.stone = true;
+                marker.marker = (GameObject)Instantiate(markerPrefab, mv, new Quaternion());
+                
+                markers.Add(marker);
+                break; 
+            case "DMARKER":
+
+                break;
+            case "PRINTF":
+
+                break;
+            default:
+                Debug.LogWarning("Command: " + command[0] + " not recogniced!");
+                break;
+        }
+	}
+}
---
+++ ASCLabVisualizer/Assets/CreateWorldOnStartup.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using System.IO;
+
+public class CreateWorldOnStartup : MonoBehaviour {
+
+    public string wayFile;
+    public List<List<string>> commands;
+    public GameObject FloorPrefab;
+    public GameObject WallPrefab;
+    public GameObject EndPrefab;
+    public GameObject playerObject;
+    int startX, startY, endX, endY;
+
+	// Use this for initialization
+	void Start () {
+        string[] args = Environment.GetCommandLineArgs();
+        if (args.Length <= 2)
+        {
+            Debug.LogWarning("No start parameters set, appling default parameter");
+            wayFile = @"C:/Projects/testWay.txt";
+        }
+        else
+        {
+            if (args[1] != "-way")
+            {
+                Debug.LogWarning("No -way in Start options found, using default path");
+                wayFile = @"C:/Projects/testWay.txt";
+            }
+            else
+            {
+                wayFile = args[2];
+            }
+        }
+        if (!File.Exists(wayFile))
+        {
+            Debug.LogError("Could not open way file " + wayFile + ", aborting");
+            return;
+        }
+        StreamReader sr = new StreamReader(wayFile);
+        string lvlFile = sr.ReadLine();
+        //Create the list of commands
+        commands = new List<List<string>>();
+        while(!sr.EndOfStream)
+        {
+            List<string> command = new List<string>();
+            command.AddRange(sr.ReadLine().Split());
+            commands.Add(command);
+        }
+        sr.Close();
+        Debug.Log("Read commands");
+
+        //Load the level
+        if(!File.Exists(lvlFile))
+        {
+            Debug.LogError("Could not load Level File!");
+            return;
+        }
+        Debug.Log("Opening Level File: " + lvlFile);
+        sr = new StreamReader(lvlFile);
+        string[] lvlSize = sr.ReadLine().Split();
+        int lvlXSize = Int32.Parse(lvlSize[0]);
+        int lvlYSize = Int32.Parse(lvlSize[1]);
+        Vector3 currentPos = new Vector3(0, 0, 0);
+        Vector2 startPos;
+        int lineNr = 0;
+        while(!sr.EndOfStream)
+        {
+            string line = sr.ReadLine();
+            for(int i = 0; i < lvlXSize; i++)
+            {
+                if (line[i] == ' ' || line[i] == 'S' || line[i] == 'E')
+                {
+                    GameObject floor = (GameObject)Instantiate(FloorPrefab, currentPos, new Quaternion());
+                }
+                if(line[i] == 'E')
+                {
+                    GameObject end = (GameObject)Instantiate(EndPrefab, currentPos, new Quaternion());
+                    endX = i;
+                    endY = lineNr;
+                }
+                if(line[i]== '#')
+                {
+                    GameObject wall = (GameObject)Instantiate(WallPrefab, currentPos, new Quaternion());
+                }
+                if (line[i] == 'S')
+                {
+                    startX = i;
+                    startY = lineNr;
+                    playerObject.transform.position= new Vector3(startX, 0, -startY);//- because the grid is inverted
+                }
+                currentPos.x += 1;
+            }
+            currentPos.z -= 1;
+            currentPos.x = 0;
+            
+            lineNr++;
+        }
+        Debug.Log("Read lvlFile");
+        GameObject player = GameObject.Find("User");
+        CommandExecutioner ce = (CommandExecutioner)player.GetComponent("CommandExecutioner");
+        ce.commands = commands;
+        ce.running = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+}
