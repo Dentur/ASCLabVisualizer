9e1fc31f071c9ad4ef4a0ef8ecf3903bfbb57f80
Sebastian Venhuis <sebastian@venhuis.com>
2017-01-09 13:58:17 +0100

    free camera + build

---
+++ ASCLabVisualizer/Assets/KameraSwitch.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KameraSwitch : MonoBehaviour {
+
+    public GameObject cameraUser;
+    public GameObject cameraFly;
+
+    private bool cameraUserInUse = true;
+    private bool prellschutz = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Jump") >= 0.7)
+        {
+            if (!prellschutz)
+            {
+                prellschutz = true;
+                cameraUserInUse = !cameraUserInUse;
+                Debug.Log("Switching Camera");
+                if (cameraUserInUse)
+                {
+                    cameraUser.SetActive(false);
+                    cameraFly.SetActive(true);
+
+                    Cursor.lockState = CursorLockMode.Confined;
+                }
+                else
+                {
+                    cameraUser.SetActive(true);
+                    cameraFly.SetActive(false);
+
+                    Cursor.lockState = CursorLockMode.None;
+                }
+            }
+        }
+        else
+            prellschutz = false;
+	}
+}
